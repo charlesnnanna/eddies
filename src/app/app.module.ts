8cde9236e36f4a123541d41291c38b30a0f8f91c
unknown <Metaphor1667>
2022-01-13 03:07:11 -0800

    Updated Services Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './home/hero.component';
+import { ServicesComponent } from './home/services.component';
 import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeroComponent
+    HeroComponent,
+    ServicesComponent,
   ],
   imports: [
     BrowserModule,
